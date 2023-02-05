562275431ef7c5e02343b122a5af9cd5d4788b2e
RydalisDarx <allplayerof@gmail.com>
2023-02-05 02:13:52 -0800

    a

--- Assets/Scripts/Menus/ProfileChange.cs
+++ Assets/Scripts/Menus/ProfileChange.cs
@@ -6,28 +6,17 @@ using TMPro;
 
 public class ProfileChange : MonoBehaviour
 {
-    [SerializeField] public PlayerType playerProperties;
+    [SerializeField] public PlayerType player;
 
     [SerializeField] public TextMeshPro traitsText;
 
-    public Action OnPassive;
-
     void Awake()
     {
-        traitsText.text = playerProperties.trait.ToString();
+        traitsText.text = "";
+        foreach(Trait t in player.traits)
+        {
+            traitsText.text += t.ToString() + " ";
+        }
+        
     }
-
-    //Instantiates PlayerType for the sake of testing 
-    public void GetCharacterStatistics(PlayerType playerProperties)
-    {
-        PlayerType type = Instantiate(playerProperties);
-        OnPassive += playerProperties.setCharType;
-        OnPassive += type.MoveSkill;
-        OnPassive += type.InheritedSkill;
-        OnPassive.Invoke();
-        OnPassive -= playerProperties.setCharType;
-        OnPassive -= type.MoveSkill;
-        OnPassive -= type.InheritedSkill;
-    }
-
 }
